2347b9a5ed03a896cb49120d9ad29d6dbabcc4f2
Risadeiro <malcolm.osoares@gmail.com>
2026-05-04 16:11:11 -0300

    remove unnecessary comments

--- resources/api_principal.py
+++ resources/api_principal.py
@@ -1,15 +1,1 @@
 routes = {"insurance_company_api": "http://localhost:8080"}
-# CREATE QUOTE
-# {'id': '9b79f4db-1142-4eed-8350-d2e2b66eb04d',
-# 'price': 1.9,
-# 'expire_at': '2026-05-04',
-# 'age': 0,
-# 'sex': 'n'}
-#  CREATE POLICY
-#  {'quotation_id': 'ca9f02a0-e1da-446d-9b0e-163ba4231f5a',
-#  'name': 'person',
-#  'sex': 'n',
-#  'date_of_birth': '2025-09-01',
-#  'id': 'a5602164-eccf-4f3b-8e56-010654ed13d5'}
-# GET POLICY
-# {'quotation_id': 'ca9f02a0-e1da-446d-9b0e-163ba4231f5a', 'name': 'person', 'sex': 'n', 'date_of_birth': '2025-09-01', 'id': 'a5602164-eccf-4f3b-8e56-010654ed13d5'}
